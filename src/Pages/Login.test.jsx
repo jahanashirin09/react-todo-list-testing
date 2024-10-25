6dc0c47100220ab31d0d186ce810f38e096e2585
jahanashirin09 <jahanashrn411@gmail.com>
2024-10-25 09:09:11 +0530

    testing in google auth

--- src/Pages/Login.test.jsx
+++ src/Pages/Login.test.jsx
@@ -1,5 +1,5 @@
-import { describe, expect, it, beforeEach ,vi} from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, it, beforeEach } from "vitest";
+import { render, screen,} from "@testing-library/react";
 import  Login  from "./Login";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
@@ -20,7 +20,7 @@ describe("Login Page", () => {
     </BrowserRouter>
   );
   beforeEach(() => {
-    localStorage.clear(); // Clear local storage before each test
+    localStorage.clear(); 
   });
 
   it("renders the login form", () => {
@@ -55,65 +55,5 @@ describe("Login Page", () => {
   
     expect(await screen.queryByText(ERROR_MESSAGES.ACCOUNT_NOT_EXIST)).not.toBeInTheDocument();
   });
-  it('successful login redirects to list page', async () => {
-    localStorage.setItem('test@example.com', JSON.stringify({ email: 'test@example.com', password: 'password123' }));
-    const navigate = vi.fn();
-    user.setup();
-    const login_email = screen.getByPlaceholderText(/Enter Email.../i);
-    await user.clear(login_email );
-    await userEvent.type(login_email, 'test@example.com');
-    const login_password = screen.getByPlaceholderText(/Enter Password.../i);
-    await user.clear(login_password);
-    await userEvent.type(login_password, 'password123');
-    await userEvent.click(screen.getByTestId("login-btn"));
-
-    await  expect(navigate).toHaveBeenCalledWith('/listpage', expect.any(Object));
-  });
   
-
-  // it("accound doesnt exisits message when not login",async()=>{
-  //   user.setup()
-  //   const login_email=screen.getByPlaceholderText("Enter Email...")
-  //   await user.type(login_email,"jahana@gmail.com")
-  //   const login_password=screen.getByPlaceholderText("Enter Password...")
-  //   await user.type(login_password,"123456789")
-  //   const login_button=screen.getByTestId("login-btn")
-  //   await user.click(login_button)
-  //   expect("Account doesn't exisist").toBeInTheDocument();
-
-  // })
-  // it("displays the login heading", () => {
-  //   const LoginHeader = screen.getByTestId("LoginHeader");
-  //   expect(LoginHeader).toBeInTheDocument();
-  //   expect(LoginHeader).toHaveTextContent("Login");
-  // });
-  // it("rendering email and check if it is empty", async () => {
-  //   const loginlabel = screen.getAllByRole("label");
-  //   expect(loginlabel[0]).toBeInTheDocument();
-  //   expect(loginlabel[0]).toHaveTextContent("Email");
-  //   const login_button = screen.getByRole("button");
-  //   user.setup();
-  //   await user.click(login_button);
-  //   expect(screen.queryByText("email is required")).toBeInTheDocument();
-  // });
-  // it("display the label password", async () => {
-  //   const loginlabel = screen.getAllByRole("label");
-  //   expect(loginlabel[1]).toHaveTextContent("Password");
-  //   const login_button = screen.getByRole("button");
-  //   user.setup();
-  //   await user.click(login_button);
-  //   expect(screen.queryByText("password is required")).toBeInTheDocument();
-  // });
-  // it("render the login button", async () => {
-  //   const loginButton = screen.getByRole("button");
-  //   expect(loginButton).toBeInTheDocument();
-  //   user.setup();
-  //   await user.click(loginButton);
-  // });
-  // it("rendering link to sighn up", () => {
-  //   expect(screen.getByText("Don't have an account?")).toBeInTheDocument();
-  //   const link = screen.getByRole("link");
-  //   expect(link).toBeInTheDocument();
-  //   expect(link).toHaveAttribute("href", "/signUp");
-  // });
 });
